f6224730ec4385d0b3151c303d3dd8003fb66e93
jaanav23-thecoder <jaanav23@gmail.com>
2025-10-03 22:38:37 +0530

    Add files via upload

---
+++ simple chatbot project.py
@@ -0,0 +1,8 @@
+print ("hello i'm a simple chatbot. please interact with hi or goodbye")
+user_input = input()
+if user_input == "hi":
+    print("hello,hope u are doing fine!")
+elif user_input == "goodbye":
+    print ("see you again , have a great one!")
+else:
+    print("i'm a simple chatbot , i can't understand that yet!")
